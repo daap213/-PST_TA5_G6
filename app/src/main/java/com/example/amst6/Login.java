dd2811f021f447b49626e92b78bf9017054a1dff
Bryan Cali <bcali@espol.edu.ec>
2020-08-13 18:28:47 -0500

    Creacion del Login.java de la ilustración 1, "Bienvenida", se añadio el boton que permita continuar a la siguiente parte

--- app/src/main/java/com/example/amst6/Login.java
+++ app/src/main/java/com/example/amst6/Login.java
@@ -2,7 +2,9 @@ package com.example.amst6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void continuar(View view) {
+        Intent i = new Intent(this, LoginUsuario.class );
+        startActivity(i);
+    }
+
+
 }
