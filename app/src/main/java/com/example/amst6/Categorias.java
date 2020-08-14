7c102cb1377c9ab8baad8c6db610a3ca077ff4ce
Daniel Alvarado <danapela@espol.edu.ec>
2020-08-14 01:15:13 -0500

    Creación de la ventana categoria

---
+++ app/src/main/java/com/example/amst6/Categorias.java
@@ -0,0 +1,31 @@
+package com.example.amst6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Categorias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorias);
+    }
+    public void botones(View view) {
+        switch(view.getId()){
+            case R.id.btn1:
+                break;
+            case R.id.btn2:
+                break;
+            case R.id.btn3:
+                break;
+            case R.id.btn4:
+                break;
+            case R.id.btn5:
+                break;
+            case R.id.btn6:
+                break;
+        }
+    }
+}
